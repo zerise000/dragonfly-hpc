329a99fdf51697f6430b6fb9b24feacb89a88a17
usioumeo <alessio.zeni01@gmail.com>
2025-01-06 21:19:40 +0100

    openmpi working

--- code/parallel-utils.h
+++ code/parallel-utils.h
@@ -1,5 +1,5 @@
-#ifndef DA_UTILS
-#define DA_UTILS
+#ifndef DA_PARALLEL
+#define DA_PARALLEL
 
 float* init_array_parallel(unsigned int dimensions, float range_max,int nr_threads);
 void parallel_sum(float* cumulated_pos,float* average_speed,float* positions,float* speeds,unsigned int N,unsigned int dimensions,unsigned int tot_threads);
