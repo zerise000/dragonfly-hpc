89380216bd1d1add4e64f4aa0b35ba47bb2ce14e
usioumeo <alessio.zeni01@gmail.com>
2025-01-23 22:56:21 +0100

    send

--- code/bin_serial.c
+++ code/bin_serial.c
@@ -7,8 +7,8 @@
 #include <string.h>
 #include <time.h>
 
-float *dragonfly_serial_compute(Parameters p, Weights w, Fitness f, unsigned int srand
-                                );
+float *dragonfly_serial_compute(Parameters p, Weights w, Fitness f,
+                                unsigned int srand);
 
 #ifndef DA_SERIAL_LIB
 int main(int argc, char *argv[]) {
@@ -18,13 +18,14 @@ int main(int argc, char *argv[]) {
 
   // set parameters
   Parameters p = parameter_parse(argc, argv);
-  unsigned int seed=time(NULL);
-  float *shifted_tmp = malloc(sizeof(float)*p.dim);
-  float *shifted_rotation = malloc(sizeof(float)*p.dim*p.dim);
+  unsigned int seed = time(NULL);
+  float *shifted_tmp = malloc(sizeof(float) * p.dim);
+  float *shifted_rotation = malloc(sizeof(float) * p.dim * p.dim);
   float *shifted_shift = init_array(p.dim, 100.0, &seed);
   init_matrix(shifted_rotation, 100.0, p.dim, &seed);
 
-  init_shifted_fitness(shifted_tmp, shifted_rotation, shifted_shift, rastrigin_fitness);
+  init_shifted_fitness(shifted_tmp, shifted_rotation, shifted_shift,
+                       rastrigin_fitness);
 
   Fitness fitness = shifted_fitness;
   /* prev 6051
@@ -47,22 +48,9 @@ New min -941
 0.000000
  */
   float wi[16] = {
-    0.000000,
-4.967649,
-0.000000,
-0.000100,
-0.285827,
-0.000000,
-2.293534,
-2.777018,
-0.000000,
-0.000100,
-0.000000,
-0.000100,
-0.000000,
-1.027528,
-3.797735,
-0.000000,
+      0.000000, 4.967649, 0.000000, 0.000100, 0.285827, 0.000000,
+      2.293534, 2.777018, 0.000000, 0.000100, 0.000000, 0.000100,
+      0.000000, 1.027528, 3.797735, 0.000000,
 
   };
   Weights w = {
@@ -95,7 +83,6 @@ New min -941
 }
 #endif
 
-
 unsigned int raw_sendrecv_shift;
 void raw_sendrecv(Message *send, unsigned int destination, Message *recv_buffer,
                   unsigned int source, void *data_raw) {
@@ -107,23 +94,23 @@ void raw_sendrecv(Message *send, unsigned int destination, Message *recv_buffer,
 }
 
 // take timing not including IO
-float *dragonfly_serial_compute(Parameters p, Weights w, Fitness fitness, unsigned int srand
-                                ) {
+float *dragonfly_serial_compute(Parameters p, Weights w, Fitness fitness,
+                                unsigned int srand) {
   unsigned int dim = p.dim;
   unsigned int chunks = p.chunks;
 
   Dragonfly *d = malloc(sizeof(Dragonfly) * p.chunks);
-  //allocate problem
+  // allocate problem
   for (unsigned int i = 0; i < p.chunks; i++) {
-    d[i] = dragonfly_new(p.dim, p.n, p.chunks, i, p.iterations, 100.0, w, fitness,
-                         i+srand);
+    d[i] = dragonfly_new(p.dim, p.n, p.chunks, i, p.iterations, 100.0, w,
+                         fitness, i + srand);
     dragonfly_alloc(&d[i]);
   }
 
   float *best = malloc(sizeof(float) * dim);
   memcpy(best, d[0].positions, dim * sizeof(float));
   float best_fitness = d[0].fitness(best, dim);
-  //printf("starting fitness %f\n", best_fitness);
+  // printf("starting fitness %f\n", best_fitness);
 
   unsigned int joint_chunks = 1;
   int log_chunks = 0;
@@ -133,7 +120,8 @@ float *dragonfly_serial_compute(Parameters p, Weights w, Fitness fitness, unsign
     log_chunks++;
   }
 
-  unsigned int update_chunk_steps = (p.iterations + log_chunks) / (log_chunks + 1);
+  unsigned int update_chunk_steps =
+      (p.iterations + log_chunks) / (log_chunks + 1);
   Message *messages = malloc(sizeof(Message) * chunks * 2);
   raw_sendrecv_shift = chunks;
   // for each iteration
@@ -187,7 +175,7 @@ float *dragonfly_serial_compute(Parameters p, Weights w, Fitness fitness, unsign
   }
   memcpy(best, messages[0].next_food, dim * sizeof(float));
   free(messages);
-  //free d
+  // free d
 
   for (unsigned int i = 0; i < p.chunks; i++) {
     dragonfly_free(d[i]);
--- code/bin_training_parameters.c
+++ code/bin_training_parameters.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include<math.h>
 #define DA_SERIAL_LIB
 #include "bin_serial.c"
 
-float eval(float *wi){
-    Weights w = {
+float eval(float *wi) {
+  Weights w = {
       // exploring
       .al = {wi[0], wi[1]},
       .cl = {wi[2], wi[3]},
@@ -22,83 +22,98 @@ float eval(float *wi){
       .ll = {wi[12], wi[13]},
       .max_speedl = {wi[14], wi[15]},
   };
-  Parameters p ={.n=100, .dim=10, .chunks=8, .iterations=1000};
-  
+  Parameters p = {.n = 100, .dim = 10, .chunks = 32, .iterations = 1000};
 
   Fitness fitness = shifted_fitness;
-  float avg=0.0;
-  int N = 50;
-  for(int i =0; i<N; i++){
+  int N = 100;
+  float *array = malloc(sizeof(float)*N);
+  for (int i = 0; i < N; i++) {
     unsigned int seed = rand();
 
-    float *shifted_tmp = malloc(sizeof(float)*p.dim);
-    float *shifted_rotation = malloc(sizeof(float)*p.dim*p.dim);
-    float *shifted_shift = init_array(p.dim, 100.0, &seed);
+    float *shifted_tmp = malloc(sizeof(float) * p.dim);
+    float *shifted_rotation = malloc(sizeof(float) * p.dim * p.dim);
+    float *shifted_shift = init_array(p.dim, 80.0, &seed);
     init_matrix(shifted_rotation, 100.0, p.dim, &seed);
-    
-    init_shifted_fitness(shifted_tmp, shifted_rotation, shifted_shift, rastrigin_fitness);
-    
+
+    init_shifted_fitness(shifted_tmp, shifted_rotation, shifted_shift,
+                         rastrigin_fitness);
+
     float *res = dragonfly_serial_compute(p, w, fitness, seed);
-    //printf("%f\n", fitness(res, p.dim));
-    avg+= fitness(res, p.dim);
+     printf("%f\n", fitness(res, p.dim));
+    array[i] = fitness(res, p.dim);
     free(shifted_tmp);
     free(shifted_rotation);
     free(shifted_shift);
     free(res);
   }
- //printf("avg: %f\n", avg);
-    return avg/N;
+  float std=0.0, avg=0.0;
+  for(int i=0; i<N; i++){
+    avg+=array[i];
+  }
+  avg/=N;
+  for(int i=0; i<N; i++){
+    std+=(float)sqrt((double)(array[i]-avg)*(array[i]-avg));
+  }
+  std/=N;
+  free(array);
+  printf("avg: %f\n std %f", avg, std);
+  return avg / N;
 }
 
-int main(){
-    /*
-    Weights w = {
-      // exploring
-      .al = {0.0, 0.00},
-      .cl = {0.00, 0.45},
-      // swarming
-      .sl = {0.0, 0.0},
-      .fl = {0.7, 0.9},
-      .el = {0.0, 0.0},
-      .wl = {0.9, 0.4},
-      .ll = {0.11, 0.3},
-      .max_speedl = {1.7, 2.15
-  };
-    */
-    srand(time(NULL));
-    float best[16] = {0.0, 0.05, 0.12, 0.33, 0.000000, 0.04, 0.85, 0.95, 0.000000, 0.0, 0.86, 0.41, 0.05, 0.19, 2.4, 1.4,};
-    float best_fitness = eval(best);
-    printf("starting: %f\n", best_fitness);
-    float cur[16];
-    unsigned int seed = rand();
-    while(true){
-        for(int i=0; i<16; i++){
-            cur[i]=best[i];
-            //if(rand()%8==0){
-                cur[i]=best[i]+RAND_FLOAT(0.05, &seed);
-                if(cur[i]<0.0){
-                    cur[i]=0.0;
-                }
-            //}
-            
-        }
-        float fit = eval(cur);
-        if(fit>best_fitness){
-            best_fitness=fit;
-            printf("New min %f\n", best_fitness);
-            for(int i=0; i<16; i++){
-                printf("%f, ", cur[i]);
-            }
-            printf("\n");
-            memcpy(best, cur, sizeof(float)*16);
-        }
-    }
+int main() {
+  /*
+  Weights w = {
+    // exploring
+    .al = {0.0, 0.00},
+    .cl = {0.00, 0.45},
+    // swarming
+    .sl = {0.0, 0.0},
+    .fl = {0.7, 0.9},
+    .el = {0.0, 0.0},
+    .wl = {0.9, 0.4},
+    .ll = {0.11, 0.3},
+    .max_speedl = {1.7, 2.15
+};
+  */
+  srand(time(NULL));
+  float best[16] = {
+      0.000000, 4.967649, 0.000000,  0.000100, 0.285827, 0.000000,
+      2.293534, 2.777018, 0.000000,  0.000100, 0.000000, 0.000100,
+      0.000000, 1.027528, 3.797735, 0.000000,
 
+  };
+  float best_fitness = eval(best);
+  /*printf("starting: %f\n", best_fitness);
+  float cur[16];
+  unsigned int seed = rand();
+  while (true) {
+    for (int i = 0; i < 16; i++) {
+      cur[i] = best[i];
+      // if(rand()%8==0){
+      cur[i] = best[i] + RAND_FLOAT(0.05, &seed);
+      if (cur[i] < 0.0) {
+        cur[i] = 0.0;
+      }
+      //}
+    }
+    float fit = eval(cur);
+    if (fit > best_fitness) {
+      best_fitness = fit;
+      printf("New min %f\n", best_fitness);
+      for (int i = 0; i < 16; i++) {
+        printf("%f, ", cur[i]);
+      }
+      printf("\n");
+      memcpy(best, cur, sizeof(float) * 16);
+    }
+  }*/
 }
 /*
 New min -353743.656250
-0.181880 -0.102300 -0.035865 0.361603 0.596336 0.018159 0.651550 0.487240 -0.064944 0.030771 0.898261 0.121631 0.063970 0.295120 1.880680 2.105216
+0.181880 -0.102300 -0.035865 0.361603 0.596336 0.018159 0.651550 0.487240
+-0.064944 0.030771 0.898261 0.121631 0.063970 0.295120 1.880680 2.105216
 
 New min -1655767.000000
-0.343304 0.120054 0.147655 0.456489 0.523672 0.009136 0.579197 0.649009 0.000000 0.011433 0.626118 0.226548 0.070795 0.611347 2.347073 1.677076 
+0.343304 0.120054 0.147655 0.456489 0.523672 0.009136 0.579197 0.649009 0.000000
+0.011433 0.626118 0.226548 0.070795 0.611347 2.347073 1.677076
 */
