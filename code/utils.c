9764171a5a1f4edc1bca686535ed3362af5591f3
zerise000 <mattysan02@gmail.com>
2025-02-14 13:59:06 +0100

    fixed openMP accumulate implementation

--- code/utils.c
+++ code/utils.c
@@ -394,37 +394,145 @@ void dragonfly_compute_step(Dragonfly *d, float *average_speed,
   weights_step(&d->w);
 }*/
 
+void inner_computation_accumulate(Dragonfly* d,float* cumulated_pos,float* cumulated_speed,float* next_food_fitness,
+																	float* next_enemy_fitness,float* best_fitness,
+																	unsigned int* indexes,unsigned int base,
+																	unsigned int limit,unsigned int dim,unsigned int seed){
+		
+		for (unsigned int k = base; k < limit; k++) {
+			float* iter_pos = d->positions + dim * k;
+			float* iter_speed = d->speeds + dim * k;
+			sum_assign(cumulated_pos,iter_pos,dim);
+			sum_assign(cumulated_speed,iter_speed,dim);
+						
+			float fitness = d->fitness(iter_pos, &seed, dim);
+
+			if (fitness > *next_food_fitness) {
+				indexes[0] = k;
+				*next_food_fitness = fitness;
+			}
+			if (fitness < *next_enemy_fitness) {
+				indexes[1] = k;
+				*next_enemy_fitness = fitness;
+			}
+			if (fitness > *best_fitness) {
+				indexes[2] = k;
+				*best_fitness = fitness;
+			}
+		}
+
+}
+
+void update_status(ComputationStatus* status,Dragonfly* d,float* best,float* best_fitness,unsigned int* indexes,unsigned int dim){
+		memcpy(status->next_food, d->positions + dim*indexes[0], sizeof(float) * dim);
+		memcpy(status->next_enemy, d->positions + dim*indexes[1], sizeof(float) * dim);
+		memcpy(best, d->positions + dim*indexes[2], sizeof(float) * dim);
+
+		status->next_food_fitness = d->fitness(status->next_food,&d->seed,dim);
+		status->next_enemy_fitness = d->fitness(status->next_enemy,&d->seed,dim);
+		*best_fitness = d->fitness(best,&d->seed,dim);
+}
+
 void computation_accumulate(ComputationStatus *status, Dragonfly *d,
                             float *best, float *best_fitness,
                             unsigned int NUM_THREADS) {
-  (void)(NUM_THREADS);
+
   unsigned int dim = d->dim;
-  zeroed(status->cumulated_pos, dim);
+	zeroed(status->cumulated_pos, dim);
   zeroed(status->cumulated_speeds, dim);
   memcpy(status->next_enemy, d->positions, sizeof(float) * dim);
   memcpy(status->next_food, d->positions, sizeof(float) * dim);
   status->next_enemy_fitness = d->fitness(status->next_enemy, &d->seed, dim);
   status->next_food_fitness = status->next_enemy_fitness;
-  status->n = 0;
-  for (unsigned int k = 0; k < d->N; k++) {
-    float *cur_pos = d->positions + dim * k;
-    sum_assign(status->cumulated_pos, cur_pos, dim);
-    sum_assign(status->cumulated_speeds, d->speeds + dim * k, dim);
-    float fitness = d->fitness(cur_pos, &d->seed, dim);
-    if (fitness > status->next_food_fitness) {
-      memcpy(status->next_food, cur_pos, sizeof(float) * dim);
-      status->next_food_fitness = fitness;
-    }
-    if (fitness < status->next_enemy_fitness) {
-      memcpy(status->next_enemy, cur_pos, sizeof(float) * dim);
-      status->next_enemy_fitness = fitness;
-    }
-    if (fitness > *best_fitness) {
-      memcpy(best, cur_pos, sizeof(float) * dim);
-      *best_fitness = fitness;
-    }
-    status->n += 1;
-  }
+  status->n = d->N;
+
+	unsigned int pos_indexes[3] = {0,0,0};
+
+#ifdef USE_OPENMP
+	unsigned int rest = d->N % NUM_THREADS;
+  unsigned int ratio = d->N / NUM_THREADS;
+	unsigned int g_seed = rand_r(&d->seed);
+
+#pragma omp parallel num_threads(NUM_THREADS)
+	{
+		
+		unsigned int rank = omp_get_thread_num();
+    unsigned int base = ratio * rank;
+    unsigned int limit = ratio * (rank + 1);
+		unsigned int local_seed = g_seed+rank;
+
+		float local_cumulated_pos[dim];
+		float local_cumulated_speeds[dim];
+
+
+		unsigned int local_indexes[3] = {base,base,base};
+
+		float next_enemy_fitness;
+		float next_food_fitness;
+		float local_best_fitness;
+		float *cur_pos = d->positions + dim*base;
+		
+		next_enemy_fitness = d->fitness(cur_pos,&local_seed,dim);
+
+		next_food_fitness = next_enemy_fitness;
+		local_best_fitness = next_enemy_fitness;
+
+		zeroed(local_cumulated_pos,dim);
+		zeroed(local_cumulated_speeds,dim);
+		
+		inner_computation_accumulate(d,local_cumulated_pos,local_cumulated_speeds,&next_food_fitness,
+																	&next_enemy_fitness,&local_best_fitness,
+																	local_indexes,base,
+																	limit,dim,local_seed);
+
+
+#pragma omp critical
+		{
+			sum_assign(status->cumulated_pos,local_cumulated_pos,dim);
+			sum_assign(status->cumulated_speeds,local_cumulated_speeds,dim);
+
+			if (next_food_fitness > status->next_food_fitness) {
+				pos_indexes[0] = local_indexes[0];
+				status->next_food_fitness = next_food_fitness;
+			}
+			if (next_enemy_fitness < status->next_enemy_fitness) {
+				pos_indexes[1] = local_indexes[1];
+				status->next_enemy_fitness = next_enemy_fitness;
+			}
+			if (local_best_fitness > *best_fitness) {
+				pos_indexes[2] = local_indexes[2];
+				*best_fitness = local_best_fitness;
+			}
+		}
+
+	}
+
+	update_status(status,d,best,best_fitness,pos_indexes,dim);
+	
+
+	if(rest!=0){
+
+    unsigned int r_base = ratio * NUM_THREADS;
+		unsigned int local_seed = g_seed + r_base;
+
+		inner_computation_accumulate(d,status->cumulated_pos,status->cumulated_speeds,&status->next_food_fitness,
+																	&status->next_enemy_fitness,best_fitness,
+																	pos_indexes,r_base,
+																	d->N,dim,local_seed);
+
+
+		update_status(status,d,best,best_fitness,pos_indexes,dim);
+	}
+#else
+  (void)(NUM_THREADS);
+  
+	inner_computation_accumulate(d,status->cumulated_pos,status->cumulated_speeds,&status->next_food_fitness,
+																	&status->next_enemy_fitness,best_fitness,
+																	pos_indexes,0,d->N,dim,d->seed);
+
+	update_status(status,d,best,best_fitness,pos_indexes,dim);
+
+#endif
 }
 
 /*
